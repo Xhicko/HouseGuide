8db772acbf2f09c6a07bbd015ed561d7e9c1f049
Xhicko <otagadaisaac@gmail.com>
2024-01-30 11:08:34 +0100

    Edited the code
    I  removed the Zoom in & Zoom out feature

--- index.js
+++ index.js
@@ -26,26 +26,24 @@ function toggleExpandState(){
 toggleExpandState()
 
 
-const nextMapOption = document.querySelector('.bi-plus-lg')
-const retunMapOption = document.querySelector('.bi-dash-lg')
 const map = document.querySelector('#Content')
 const nextIndicator = document.querySelector('.Satelite')
 const returnIndicator = document.querySelector('.Default')
 
-nextMapOption.addEventListener('click', ()=>{
+returnIndicator.addEventListener('click', ()=>{
+   returnIndicator.style.display = 'none'
+   nextIndicator.style.display = 'flex'
    map.style.background = 'url("https://i.stack.imgur.com/Bxlnz.jpg")';
    map.style.backgroundSize = 'cover'
    map.style.backgroundPosition = 'center'
-   nextIndicator.style.display = 'flex'
-   nextIndicator.style.borderRadius = '10px'
-   returnIndicator.style.display = 'none'
+
+   nextIndicator.addEventListener('click', ()=>{
+      nextIndicator.style.display = 'none'
+      returnIndicator.style.display = 'flex'
+      map.style.background = 'url("https://img.freepik.com/free-vector/colored-city-map-with-streets-park_23-2148318250.jpg?w=740&t=st=1705770581~exp=1705771181~hmac=e48ccdfe3900cf0aa3eab70052d91754fd971755c8c49a8925b8038277ccd541")';
+      map.style.backgroundSize = 'cover'
+      map.style.backgroundPosition = 'center'
+   })
 })
 
-retunMapOption.addEventListener('click', ()=>{
-   map.style.background = 'url("https://img.freepik.com/free-vector/colored-city-map-with-streets-park_23-2148318250.jpg?w=740&t=st=1705770581~exp=1705771181~hmac=e48ccdfe3900cf0aa3eab70052d91754fd971755c8c49a8925b8038277ccd541")';
-   map.style.backgroundSize = 'cover'
-   map.style.backgroundPosition = 'center'
-   returnIndicator.style.display = 'flex'
-   returnIndicator.style.borderRadius = '10px'
-   nextIndicator.style.display = 'none'
-})
+
